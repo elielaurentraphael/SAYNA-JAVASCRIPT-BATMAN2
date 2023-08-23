6df5db17bebe2f68dc66ae4d3ce61e2bc8c57723
elielaurentraphael <raphelaur@gmail.com>
2023-08-23 23:24:32 +0300

    commit n°23: --rédaction du fichier game.html en prenant index.html comme modèle --même opération pour le fichier _game.scss; _social-lateral.scss; _nav.scss; _foot-footer.scss; _flecheDown.scss

--- Js/game.js
+++ Js/game.js
@@ -1,1 +1,21 @@
+$(".btn1").click(function () {
+	console.log("OK !");
+	$(".quiz").hide();
+});
 
+$(".btn2").click(function () {
+	console.log("OK !");
+	$(".quiz").show();
+});
+
+$(".btn3").click(function () {
+	console.log("OK !");
+	$(".startQuiz").hide();
+	$(".flecheDown.down").hide();
+});
+
+$(".btn4").click(function () {
+	console.log("OK !");
+	$(".startQuiz").show();
+	$(".flecheDown.down").show();
+});
