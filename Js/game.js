fa78ba86c1012c1b896d9e33a6822e928ece5489
elielaurentraphael <raphelaur@gmail.com>
2023-08-25 00:17:39 +0300

    commit n°25: Modifications --fichier index.html ligne 22 #home --fichier game.html ligne 23 #game, ligne 28 #game, ligne 38 #foot, ligne 62 écriture question, lignes 66 à 81 input checkbox label bouton démarrer le quiz, ligne 99 #foot --fichier _home.scss lignes 119 à 122 menu home --fichier _game.scss lignes 44 à 47 animation des class,  lignes 126 à 129 menu game, lignes 152 à 154 quiz display none, lignes 190 à 206 styles pour les cases de réponses, lignes 210 à 219 règles pour le bouton démarrer le quiz  question suivante --fichier game.js click du bouton next fadeOut des class startQuiz/flecheDowndown fadeIn de la class quiz changement de l'étiquette du bouton next

--- Js/game.js
+++ Js/game.js
@@ -1,3 +1,4 @@
+/*
 $(".btn1").click(function () {
 	console.log("OK !");
 	$(".quiz").hide();
@@ -18,4 +19,34 @@ $(".btn4").click(function () {
 	console.log("OK !");
 	$(".startQuiz").show();
 	$(".flecheDown.down").show();
+});
+
+$(document).ready(function () {
+	$(window).scroll(function () {
+		let scroll = $(window).scrollTop();
+		$(".social-lateral").css("top", scroll + "px");
+		console.log(scroll);
+	});
+});
+
+$(document).ready(function () {
+	// vos scripts
+});
+*/
+
+/*setTimeout(
+	$(document).ready(function () {
+		let scroll = $(window).scrollTop();
+		$(".social-lateral").css("top", 0 + "px");
+	}),
+	1000
+);
+window.onscroll = function (event) {
+	console.log(event);
+};
+*/
+$(".next").click(function () {
+	$(".startQuiz, .flecheDown.down").fadeOut();
+	$(".quiz").fadeIn();
+	$(".next").text("question suivante");
 });
