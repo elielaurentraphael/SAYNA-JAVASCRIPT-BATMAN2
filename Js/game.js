fca673a058d743fe56b6c3d5a2a7361cbdd96a49
elielaurentraphael <raphelaur@gmail.com>
2023-08-25 22:51:28 +0300

    commit n°27: --fichier game.html ligne 80 suppression du contenu de la div class next --fichier _game.scss ligne 225 cursor pointer --fichier game.js -lignes 1 à 7 déclaration des variables -ligne 8 étiquette bouton de démarrage -lignes 15 à 17 numérotage automatique des questions -ligne 29 écouteur d'evenement click du bouton class .next -ligne 30 condition de démarrage -lignes 36 à 42 changement du numéro question réponses après click du bouton question suivante

--- Js/game.js
+++ Js/game.js
@@ -1,10 +1,20 @@
+const numer = document.querySelector(".numero");
+const questio = document.querySelector(".question");
+const labelun = document.querySelector(".res:first-child label");
+const labeldeux = document.querySelector(".res:nth-child(2) label");
+const labeltrois = document.querySelector(".res:last-child label");
+
+let bouton = document.querySelector(".next");
+bouton.textContent = "démarrer le quiz";
+
 function quiz(x) {
 	$.ajax({
 		url: "https://batman-api.sayna.space/questions",
 		datatype: "json",
 		success: function (questions) {
+			let u = questions.length;
 			let y = parseInt(x);
-			$(".numero").text(y + 1 + "/" + 12);
+			$(".numero").text(y + 1 + "/" + u);
 			$(".question").text(questions[x]["question"]);
 			$(".res:first-child label").text(questions[x]["response"][0]["text"]);
 			$(".res:nth-child(2) label").text(questions[x]["response"][1]["text"]);
@@ -16,9 +26,19 @@ function quiz(x) {
 	});
 }
 
-$(".next").click(function () {
-	$(".startQuiz, .flecheDown.down").fadeOut();
-	$(".quiz").fadeIn();
-	$(".next").text("question suivante");
-	quiz(8);
+bouton.addEventListener("click", () => {
+	if (bouton.textContent === "démarrer le quiz") {
+		$(".startQuiz, .flecheDown.down").fadeOut();
+		$(".quiz").fadeIn();
+		quiz(0);
+		bouton.textContent = "question suivante";
+	} else {
+		$(".question").empty();
+		$(".res:first-child label").empty();
+		$(".res:nth-child(2) label").empty();
+		$(".res:last-child label").empty();
+		let splitNum = $(".numero").text();
+		let letter = splitNum[0];
+		quiz(letter);
+	}
 });
