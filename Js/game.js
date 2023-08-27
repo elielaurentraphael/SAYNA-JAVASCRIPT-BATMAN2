5de368e42ba6913ee391832653f7b0c325d462d2
elielaurentraphael <raphelaur@gmail.com>
2023-08-27 22:16:51 +0300

    commit n°29: ajout des commentaires dans le fichier game.js (à suivre)

--- Js/game.js
+++ Js/game.js
@@ -41,46 +41,51 @@ function jeux(x) {
 			$(".res:first-child span").text(questions[x]["response"][0]["isGood"]); // réponse vraie ou fausse
 			$(".res:nth-child(2) span").text(questions[x]["response"][1]["isGood"]); // réponse vraie ou fausse
 			$(".res:last-child span").text(questions[x]["response"][2]["isGood"]); // réponse vraie ou fausse
-			$(".next").attr("disabled", "disabled"); // le bouton est de nouveau "enbled" après le choix de réponse
+			$(".next").attr("disabled", "disabled"); // le bouton est de nouveau "enabled" après le choix de réponse
 		},
 		error: function () {
-			console.log("Défaut de connexion !");
+			console.log("Défaut de connexion !"); // signalisation de la défaillance reseau dans la console
 		},
 	});
 }
 
+// on démarre le quiz en appuyant sur le bouton "démarrer lr quiz"
 bouton.addEventListener("click", () => {
 	if (bouton.textContent === "démarrer le quiz") {
-		$(".startQuiz, .flecheDown.down").fadeOut();
-		$(".quiz").fadeIn();
-		jeux(0);
-		bouton.textContent = "question suivante";
-		choose();
+		$(".startQuiz, .flecheDown.down").fadeOut(); // faire disparaitre l'image de batman et la flèche
+		$(".quiz").fadeIn(); // faire apparaitre la console du quiz
+		jeux(0); // la console du quiz avec la première question et la proposition des trois réponses
+		bouton.textContent = "question suivante"; // l'étiquette du bouton change "démarrer le quiz" devient "question suivante"
+		choose(); // choisir une réponse parmi les trois proposées en cochant une case
 	} else {
-		let splitNum = $(".numero").text();
+		let splitNum = $(".numero").text(); // le numéro d'une question est du type string de longueur 4 ou 5
 		let lengtha = splitNum.length;
 		let letter;
 		if (lengtha == 4) {
-			letter = splitNum[0];
+			// numéro < 10 - format: s/ss - c'est à dire longueur = 4
+			letter = splitNum[0]; // dans ce cas, on met le premier élément dans la variable letter
 		} else {
+			// dans le cas où la longueur du numéro est 5; on met le premier et deuxième éléments dans la variable letter
 			letter = splitNum[0] + splitNum[1];
 			console.log(`letter = ${letter}`);
 		}
 		if (letter == $(".nombreQuestion").text()) {
-			$(".quiz").fadeOut();
+			// si le numéro de la question = nombre de question: c'est la fin du quiz
+			$(".quiz").fadeOut(); // après le choix de la réponse, on fait disparaitre la console du quiz
 			$(".next").fadeOut();
-			$(".result").css("display", "block");
+			$(".result").css("display", "block"); // on affiche le résultat
 		} else {
+			// numéro de la question < nombre de questions, on continue le quiz
 			$(".res:first-child input").prop("checked", false);
-			$(".res:nth-child(2) input").prop("checked", false);
+			$(".res:nth-child(2) input").prop("checked", false); // on vide les cases à cocher
 			$(".res:last-child input").prop("checked", false);
-			jeux(letter);
-			choose();
+			jeux(letter); // on attaque la question suivante
+			choose(); // choix d'une réponse
 		}
 	}
 });
 
-const resInputs = document.querySelectorAll(".res input");
+const resInputs = document.querySelectorAll(".res input"); // on pointe les cases à cocher
 const span = document.querySelector(".res:last-child span");
 
 function choose() {
