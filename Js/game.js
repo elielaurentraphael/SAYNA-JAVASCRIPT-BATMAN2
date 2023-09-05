1a66cdee2d8ed6c9bdbc40548d012e6a70666761
elielaurentraphael <raphelaur@gmail.com>
2023-09-05 08:11:20 +0300

    commit n°33: --fichier game.html lignes 62 à 172: rédaction des images pour chaque question du quiz --fichier _game.scss -lignes 26 à 31: modification des règles pour le heading de la page -lignes 66 à 212: pose des règles pour l'affichage des images de chaque question du quiz -ligne 260: opacity de l'indice de la vraie réponse --fichier game.js: réfonte du fichier

--- Js/game.js
+++ Js/game.js
@@ -6,6 +6,12 @@ const labelun = document.querySelector(".res:first-child label"); // réponse po
 const labeldeux = document.querySelector(".res:nth-child(2) label"); // réponse possible  parmi les trois proposés
 const labeltrois = document.querySelector(".res:last-child label"); // réponse possible  parmi les trois proposés
 
+const resInputs = document.querySelectorAll(".res input"); // on pointe les cases à cocher
+const span = document.querySelector(".res:last-child span"); // on pointe l'indice de la vraie réponse
+const isGoods = document.querySelectorAll(".isGood"); // on sélectionne les indices de vérité
+const note = document.querySelector(".note"); // on cible l'affichage de la note
+note.textContent = 0; // on initialise la note à 0 (zéro)
+
 let bouton = document.querySelector(".next"); // bouton pour démarrer le quiz ou pour voir la question suivante
 bouton.textContent = "démarrer le quiz"; // étiquette du bouton au démarrage (l'autre étiquette est: "question suivante")
 
@@ -35,6 +41,7 @@ function jeux(x) {
 			let y = parseInt(x);
 			$(".numero").text(y + 1 + "/" + u); // affichage numéro de la question
 			console.log(`n° question = ${y + 1}`);
+			imageQuestion(y + 1);
 			$(".question").text(questions[x]["question"]); // affichage de la question
 			$(".res:first-child label").text(questions[x]["response"][0]["text"]); // réponse probable parmi les trois proposées
 			$(".res:nth-child(2) label").text(questions[x]["response"][1]["text"]); // réponse probable parmi les trois proposées
@@ -43,35 +50,68 @@ function jeux(x) {
 			$(".res:nth-child(2) span").text(questions[x]["response"][1]["isGood"]); // réponse vraie ou fausse
 			$(".res:last-child span").text(questions[x]["response"][2]["isGood"]); // réponse vraie ou fausse
 			$(".next").attr("disabled", "disabled"); // le bouton est de nouveau "enabled" après le choix de réponse
+			console.log(`Question n° ${y + 1} affichée`);
 		},
 		error: function () {
 			console.log("Défaut de connexion !"); // signalisation de la défaillance reseau dans la console
+			$(".imageQuestion-9").css("display", "block");
 		},
 	});
 }
 
+// fonction d'affichage des images suivant le numéro de la question
+
+function imageQuestion(m) {
+	if (m === 1) {
+		$(`.imageQuestion-${m}`).css("display", "block");
+	} else {
+		$(`.imageQuestion-${m - 1}`).css("display", "none");
+		$(`.imageQuestion-${m}`).css("display", "block");
+	}
+}
+
+// fonction de désactivation des cases à cocher après le choix de la réponse
+
 function disabled() {
+	console.log("3 - séquence : désactivation des cases à cocher");
 	$(".res:first-child input").prop("disabled", true);
 	$(".res:nth-child(2) input").prop("disabled", true); // on désactive les cases à cocher
 	$(".res:last-child input").prop("disabled", true);
 	questionSuivante();
 }
 
+// autorisation pour l'affichage de la question suivante
+
 function questionSuivante() {
+	//console.log("DDDDDDDDD");
 	$(".next").removeAttr("disabled");
+	console.log(
+		"4 - séquence : on peut appeler la question suivante en appuyant le bouton 'question suivante'"
+	);
 }
 
+// fonction d'activation des cases à cocher après l'affichage de la question
+
 function enabled() {
+	console.log(
+		"5 - séquence : activation des cases à cocher pour la réponse de la question n° " +
+			(parseInt(numera()) + 1)
+	);
 	$(".res:first-child input").prop("disabled", false);
 	$(".res:nth-child(2) input").prop("disabled", false); // on active les cases à cocher
 	$(".res:last-child input").prop("disabled", false);
 }
 
+// fonction d'éffacement des cochages éventuels des cases à cocher avant l'affichage de la question suivante
+
 function unchecked() {
+	console.log("6 - séquence d'éffacement des cochages éventuels");
 	$(".res:first-child input").prop("checked", false);
 	$(".res:nth-child(2) input").prop("checked", false); // on vide les cases à cocher
 	$(".res:last-child input").prop("checked", false);
 }
+
+// fonction de rappel du numéro de la question encours pour préparer le numéro de la question suivante
 
 function numera() {
 	let numero = numer.textContent;
@@ -90,44 +130,48 @@ function numera() {
 bouton.addEventListener("click", (e) => {
 	e.stopImmediatePropagation();
 	if (bouton.textContent === "démarrer le quiz") {
+		console.log("1 - séquence de démarrage");
 		$(".startQuiz, .flecheDown.down").fadeOut(); // faire disparaitre l'image de batman et la flèche
-		$(".quiz").fadeIn(); // faire apparaitre la console du quiz
-		jeux(0); // la console du quiz avec la première question et la proposition des trois réponses
+		jeux(0); // chargement de la première question et la proposition des trois réponses
 		bouton.textContent = "question suivante"; // l'étiquette du bouton change "démarrer le quiz" devient "question suivante"
+		$(".quiz").fadeIn(); // faire apparaitre la console du quiz
 		choose(); // choisir une réponse parmi les trois proposées en cochant une case
 	} else {
 		let letter;
 		letter = numera();
+		console.log(`letter = ${letter}`);
 		if (letter == $(".nombreQuestion").text()) {
 			// si le numéro de la question = nombre de question: c'est la fin du quiz
-			$(".quiz").fadeOut(); // après le choix de la réponse, l'action sur le bouton "question suivante" fait disparaitre la console du quiz et, ->
+
+			$(".quiz").fadeOut(); // après le choix de la réponse de la dernière question, l'action sur le bouton "question suivante" fait disparaitre la console du quiz et, ->
 			$(".next").fadeOut();
-			$(".result").css("display", "block"); // -> affiche le résultat
+			$(".result").css("display", "block"); // -> affiche le résultat avec la possibilité de refaire le quiz
 		} else {
 			// numéro de la question < nombre de questions, on continue le quiz
 
-			enabled();
-			unchecked();
+			enabled(); // on active les cases à cocher
+			unchecked(); // on éfface les cochages éventuels
 
-			jeux(letter); // on attaque la question suivante
+			$(".quiz").fadeOut(4000);
+			jeux(letter); // on affiche la question suivante
+			$(".quiz").fadeIn(4000);
+
+			// console.log("AAAAAAAAA");
 			choose(); // choix d'une réponse
 		}
 	}
 });
 
-const resInputs = document.querySelectorAll(".res input"); // on pointe les cases à cocher
-const span = document.querySelector(".res:last-child span"); // on pointe l'indice de la vraie réponse
-const isGoods = document.querySelectorAll(".isGood"); // on sélectionne les indices de vérité
-const note = document.querySelector(".note"); // on cible la class note
-note.textContent = 0;
-
-// fonction choisir une réponse
+// fonction pour choisir une réponse
 
 function choose() {
+	console.log("2 - séquence : on peut choisir une réponse");
 	let z = parseInt(note.textContent);
 	resInputs.forEach((resInput) => {
 		// pour chaque case à cocher
 		resInput.addEventListener("change", (e) => {
+			console.log(`question n° ${numera()} répondue`);
+			// console.log("kkkkkkkkkkk");
 			// on écoute l'événement "change"
 			let j = e.target.id;
 			if (isGoods[j].textContent === "true") {
