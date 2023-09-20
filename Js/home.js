37c0f87c58dd2477b4701932b5f3adc6ced870bf
elielaurentraphael <raphelaur@gmail.com>
2023-09-20 15:43:33 +0300

    commi n°45 - Tous les sliders défilent les images automatiquement avec un délai de 5 secondes --modification du dossier Batsliders --modification des fichiers home.js, _foot-footer.scss, _social-lateral.scss, _home.scss, index.html

--- Js/home.js
+++ Js/home.js
@@ -1,4 +1,5 @@
-// Scrolling progressif
+// SCROLLING PROGRESSIF
+
 $(document).ready(function () {
 	$(".jsScrollBut").on("click", function () {
 		// Au clic sur le bouton "VOIR LE HEROS"
@@ -16,3 +17,50 @@ $(document).ready(function () {
 		return false;
 	});
 });
+
+// TOUTES LES IMAGES DU DOSSIER "Batsliders" DEFILENT UNE à UNE AVEC UN DELAIS DE 5s
+
+// on pointe l'élément image de classa .justiceImage
+const image = document.querySelector(".justiceImage");
+// on stocke les sources des images de Batsliders dans un array "srcArray"
+const srcArray = [
+	"./Assets/Batsliders/Batslider1.png",
+	"./Assets/Batsliders/Batslider2.png",
+	"./Assets/Batsliders/Batslider3.png",
+	"./Assets/Batsliders/Batslider4.png",
+	"./Assets/Batsliders/Batslider5.png",
+	"./Assets/Batsliders/Batslider6.png",
+	"./Assets/Batsliders/Batslider7.png",
+	"./Assets/Batsliders/Batslider8.png",
+	"./Assets/Batsliders/Batslider9.png",
+	"./Assets/Batsliders/Batslider10.png",
+];
+// stockage du numéro de l'image de srcArray dans la variable variable "index" et initialisation à 0
+let index = 1;
+
+// function pour faire défiler les images toutes les 5s
+
+const loop = () => {
+	setTimeout(() => {
+		if (index < srcArray.length) {
+			affichage();
+			index++;
+			loop();
+		} else if (index === srcArray.length) {
+			affichage();
+			index = 1;
+			loop();
+		}
+		index;
+	}, 5000);
+};
+loop();
+
+function affichage() {
+	$(".justiceImage")
+		.hide()
+		.css({ left: "-25%" })
+		.attr("src", `./Assets/Batsliders/Batslider${index}.png`)
+		.show()
+		.animate({ left: "0" }, 100);
+}
